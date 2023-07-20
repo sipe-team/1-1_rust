a57fc9c352b9f94e84ed9836d8668f97a891e041
clogic <clogic@toss.im>
2023-07-21 00:37:38 +0900

    feat: ~ 눈물의... 엔딩...

--- rustlings/clogic/exercises/conversions/from_into.rs
+++ rustlings/clogic/exercises/conversions/from_into.rs
@@ -40,10 +40,27 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.is_empty() {
+            return Person::default();
+        };
+        let mut split = s.split(",");
+        if split.clone().count() != 2 {
+            return Person::default();
+        }
+        let name = split.next().unwrap();
+        let age = split.next().unwrap();
+        if name.is_empty() {
+            return Person::default()
+        }
+        if age.parse::<i32>().is_err() {
+            return Person::default()
+        }
+        return Person {
+            name: name.to_string(),
+            age: age.parse().unwrap_or(Person::default().age)
+        };
     }
 }
 
