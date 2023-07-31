3c755badc3224885a7be8e029769aab9bd0d7e24
JiHo <tlswlgh120@naver.com>
2023-07-31 23:20:15 +0900

    feat: conversions (#5)
    * feat: Update hashmaps
    
    * feat: Update quiz2
    
    * feat: Update options
    
    * feat: Update errors4.rs
    
    * feat: Update errors
    
    * feat: Update generics
    
    * feat: Update traits
    
    * feat: Update quiz3.rs
    
    * feat: Update lifetimes
    
    * feat: Update tests
    
    * feat: Update iterators
    
    * feat: Update smart_pointers
    
    * feat: Update threads1.rs
    
    * feat: Update threads.rs
    
    * feat: Update macros
    
    * feat: Update clippy
    
    * feat: Update from_into.rs
    
    * feat: Update from_into.rs
    
    * feat: Update from_str.rs
    
    * feat: Update try_from_into.rs
    
    * feat: Update as_ref_mut.rs

--- rustlings/jiho-s/exercises/conversions/from_str.rs
+++ rustlings/jiho-s/exercises/conversions/from_str.rs
@@ -31,8 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -52,6 +50,27 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            return Err(ParsePersonError::Empty);
+        }
+
+        let mut split = s.split(",");
+
+        let name = split.next().ok_or(ParsePersonError::Empty)?;
+        let age = split.next().ok_or(ParsePersonError::BadLen)?;
+
+        if let Some(_) = split.next() {
+            return Err(ParsePersonError::BadLen);
+        }
+
+        if name.is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+
+        Ok(Person {
+            name: name.to_string(),
+            age: age.parse().map_err(|e| ParsePersonError::ParseInt(e))?,
+        })
     }
 }
 
