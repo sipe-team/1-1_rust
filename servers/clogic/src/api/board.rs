5115d764502c104193300425c94990089102515a
clogic <clogic@toss.im>
2023-09-09 15:38:46 +0900

    list boards api 에러 핸들링

--- servers/clogic/src/api/board.rs
+++ servers/clogic/src/api/board.rs
@@ -1,11 +1,12 @@
 use crate::models::prelude::Board;
 use crate::settings::app::AppState;
-use actix_web::{get, web, Responder};
+use actix_web::{get, web, HttpResponse, Responder};
 use sea_orm::EntityTrait;
 
 #[get("/api/v1/boards")]
 pub async fn get_boards(data: web::Data<AppState>) -> impl Responder {
-    let _conn = &data.conn;
-    let x = Board::find().into_json().all(_conn).await.unwrap();
-    web::Json(x)
+    match Board::find().into_json().all(&data.conn).await {
+        Ok(v) => HttpResponse::Ok().json(v),
+        Err(e) => HttpResponse::InternalServerError().body(e.to_string()),
+    }
 }
